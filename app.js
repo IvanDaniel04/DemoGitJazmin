c6ce84f1e0855d204789e66d7a5d92ea5e273657
Daniel Ivan Estrada Morando <estradaivan2002@hotmail.com>
2026-02-24 20:34:10 -0600

    UPDATE metodo y mensaje de otra rama

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const msg = document.getElementById("msg");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-  msg.textContent = "Mensaje: Hola desde otra rama";
-});
-
-btn.addEventListener("click", () => {
-  msg.textContent = "Mensaje: Hola desde otra rama";
+  msg.textContent = "Mensaje: Ahora de la rama de prueba";
 });
 
